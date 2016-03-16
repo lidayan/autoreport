ff988fe9326f9f48c5fd446bc2fe66ef662509ab
lidayan <dayan.li@datayes.com>
2016-03-16 05:28:02 -0700

    add django

--- autoreport/settings.py
+++ autoreport/settings.py
@@ -75,10 +75,18 @@ WSGI_APPLICATION = 'autoreport.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #}
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE'    :'django.db.backends.mysql',
+        'NAME'      :'autoreport',
+        'USER'      :'root',
+        'PASSWORD'  :'123456',
+        'HOST'      :'127.0.0.1',
+        'PORT'      :'3306',
+    } 
 }
 
 
@@ -106,7 +114,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
