30fb913447d8213a43a8aa4ea96a6c63fc2c7e59
unknown <dayan.li@datayes.com>
2016-03-21 09:29:58 +0800

    自动发送邮件，云平台的用户和租户

--- autoreport/crons.py
+++ autoreport/crons.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 # coding=utf-8
-
+import unicodecsv as csv
 from django_cron import CronJobBase, Schedule
 from django.core.mail import EmailMultiAlternatives
+from django.db import connections
 
 class UserMasterReport(CronJobBase):
     RETRY_AFTER_FAILURE_MINS = 5
@@ -13,16 +14,86 @@ class UserMasterReport(CronJobBase):
     code = 'report.UserMasterReport'
 
     def do(self):
-        print 'running'
-        self.sendMail('good-title','asdfasdfasdf',['/tmp/test.msg'])
-        print 'end'
+        cursor = connections['usermaster'].cursor()
 
-    def sendMail(self, title, content, attches):
+        # get tenants to tenants.csv
+        tenantfile = 'tenants.txt'
+        sql = '''
+            select 
+                tp.name, t.domain, tp.contact, tp.mail, tp.phone, t.create_time
+            from 
+                tenant as t
+                join tenant_profile as tp on tp.tenant_id = t.id
+            order by
+                t.create_time desc;
+        '''
+        cursor.execute(sql)
+        fields = [desc[0] for desc in cursor.description]
+        with open(tenantfile, 'wb') as csvfile:
+            csvwriter = csv.writer(csvfile, encoding='utf-8')
+            csvwriter.writerow(fields)
+            while True:
+                row = cursor.fetchone()
+                if not row:
+                    break
+                csvwriter.writerow(row)
+
+        # get users to users.csv
+        userfile = 'users.txt'
+        sql = '''
+            SELECT 
+                t.account_id, t.username, t.domain,
+                MAX(IF(t.display_name = '电话',t.`value`,NULL)) 'phone',
+                MAX(IF(t.display_name = '邮箱',t.`value`,NULL)) 'email',
+                MAX(IF(t.display_name = '手机',t.`value`,NULL)) 'mobile',
+                t.ctime
+            FROM (
+                SELECT 
+                    p.*,f.code,f.display_name,a.username,tt.domain, a.create_time as ctime
+                FROM 
+                    account_profile p 
+                    LEFT JOIN profile_field f ON p.field_id = f.id 
+                    LEFT JOIN account a ON p.account_id = a.id
+                    left join tenant tt on a.tenant_id = tt.id
+                ) t
+            GROUP BY 
+                t.ctime desc;
+        '''
+        cursor.execute(sql)
+        fields = [ desc[0] for desc in cursor.description]
+        with open(userfile, 'wb') as csvfile:
+            spamwriter = csv.writer(csvfile, encoding='utf-8')
+            spamwriter.writerow(fields)
+            while True:
+                row = cursor.fetchone()
+                if not row:
+                    break
+                spamwriter.writerow(row)
+
+        # send mails
         from_email = 'dayan.li@datayes.com'
-        to_email = ['dayan.li@datayes.com']
+        title = '截止到发送邮件时，用户和租户统计'
+        content = '''
+Hi all:
+
+截止到发送邮件时，云平台上所有的租户和用户信息
+    所有租户如附件%s，
+    所有用户如附件%s。
+
+请打开Excel导入文本数据（数据-自文本-逗号分隔）
+
+此邮件为系统自动发送，
+如果需要调整请联系 dayan.li@datayes.com
+        ''' % (tenantfile,userfile)
+        to_email = [
+            'dayan.li@datayes.com',
+            'kecheng.lu@datayes.com',
+            'li.dai@qq.com',
+            'wei.cao@datayes.com'
+        ]
         msg = EmailMultiAlternatives(title, content, from_email, to_email)
-        for a in attches:
-            msg.attach(a)
-        msg.send()
+        msg.attach_file(tenantfile)
+        msg.attach_file(userfile)
+        msg.send(fail_silently=False)
 
 
--- autoreport/settings.py
+++ autoreport/settings.py
@@ -24,7 +24,6 @@ SECRET_KEY = '6fxkru90zl(ja%=c6le(zt-_yic@c)ucz2z*b4su$88ba&xqfb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
 ALLOWED_HOSTS = []
 
 
@@ -79,12 +78,11 @@ WSGI_APPLICATION = 'autoreport.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
-
 DATABASES = {
     #'default': {
     #    'ENGINE': 'django.db.backends.sqlite3',
     #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    #}
+    #},
     'default': {
         'ENGINE'    :'django.db.backends.mysql',
         'NAME'      :'autoreport',
@@ -92,13 +90,20 @@ DATABASES = {
         'PASSWORD'  :'123456',
         'HOST'      :'192.168.91.128',
         'PORT'      :'3306',
+    } ,
+    'usermaster': {
+        'ENGINE'    :'django.db.backends.mysql',
+        'NAME'      :'usermaster',
+        'USER'      :'paas',
+        'PASSWORD'  :'paas@123',
+        'HOST'      :'127.0.0.1',
+        'PORT'      :'3312',
     } 
 }
 
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -117,23 +122,65 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'Asia/Shanghai'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 
 # Email BACKEND
+#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST="smtp.datayes.com"
 EMAIL_PORT=25
+# EMAIL_HOST_USER='dayan.li@datayes.com'
+# EMAIL_HOST_PASSWORD='2wsx@WSX'
+
+SERVER_EMAIL = 'han.bao@datayes.com'
+ADMINS = (('Han Bao', 'han.bao@datayes.com'),)
+EMAIL_SUBJECT_PREFIX = '[django-autoreport]:'
+FAILED_RUNS_CRONJOB_EMAIL_PREFIX = "[django-cron, autoreport]: "
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-
 STATIC_URL = '/static/'
+
+
+# settings for logging
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+
+    'handlers': {
+        'console':{
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        },
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler',
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+        'autoreport': {
+            'handlers': ['console','mail_admins'],
+            'level': 'DEBUG',
+        }
+    }
+}
